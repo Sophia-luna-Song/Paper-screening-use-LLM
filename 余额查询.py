0c43a3b3d82713173d3c0306b84df957948ec176
Sophia-luna-Song <59360539+Sophia-luna-Song@users.noreply.github.com>
2025-08-25 11:06:36 +0800

    Update 余额查询.py

--- 余额查询.py
+++ 余额查询.py
@@ -7,11 +7,12 @@ payload = json.dumps({
    "hours": 24
 })
 headers = {
-   'Authorization': 'sk-qPQtTU47NOASDBYmjpu6nPMad959c6ZbR53OkufCAfFjw7yJ',
+   'Authorization': 'yours ',
    'User-Agent': 'Apifox/1.0.0 (https://apifox.com)',
    'Content-Type': 'application/json'
 }
 conn.request("POST", "/v1/query/usage_details", payload, headers)
 res = conn.getresponse()
 data = res.read()
+
 print(data.decode("utf-8"))
